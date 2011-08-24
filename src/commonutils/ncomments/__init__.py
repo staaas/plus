a84b6131d13b7f02871e0a2de050f402d0e5dcc4
Stas Rudakou <dev@nott.cc>
2011-08-25 01:36:15 +0300

    Moving to bootstrap integration.

--- src/commonutils/ncomments/__init__.py
+++ src/commonutils/ncomments/__init__.py
@@ -1,9 +1,12 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.comments.forms import CommentDetailsForm
+from django.contrib.comments.forms import CommentDetailsForm, COMMENT_MAX_LENGTH
 
 class CommentForm(CommentDetailsForm):
     email = forms.EmailField(label=_("Email address"), required=False)
+    comment       = forms.CharField(label=_('Comment'), widget=forms.Textarea({'class':'xxlarge', 'rows': 5}),
+                                    max_length=COMMENT_MAX_LENGTH)
+
 
 def get_form():
     return CommentForm

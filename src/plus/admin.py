60cd2380d7a910710c496ae1f76c4b0dc069bc60
Stas Rudakou <dev@nott.cc>
2011-08-31 09:04:05 +0300

    Improving admin interface

--- src/plus/admin.py
+++ src/plus/admin.py
@@ -1,32 +1,15 @@
 from django.contrib import admin
 from django.core.urlresolvers import reverse
-from django.contrib.admin.views.main import ChangeList
 from django.utils.translation import ugettext as _
 
 from models import Event
-
-class EventChangeList(ChangeList):
-    def __init__(self ,request, *args, **kwargs):
-        self.user = request.user
-        super(EventChangeList, self).__init__(request, *args, **kwargs)
-
-    def get_query_set(self):
-        qs = super(EventChangeList, self).get_query_set()
-
-        if not self.user.has_perm('plus.can_moderate_all'):
-            qs = qs.filter(creator=self.user)
-
-        return qs
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug_display', 'creator', 'starts_at']
     fields = ['title', 'slug', 'language', 'description', 'creator',
               'starts_at', 'seats_number', 'created_at', 'url', 'logo']
     readonly_fields = ['slug', 'created_at']
-    ordering = ['starts_at']
-
-    def get_changelist(self, *args, **kwargs):
-        return EventChangeList
+    ordering = ['-starts_at']
 
     def has_change_permission(self, request, obj=None):
         change_perm = super(EventAdmin, self).has_change_permission(
@@ -47,7 +30,16 @@ class EventAdmin(admin.ModelAdmin):
         return '<a href="%s">%s</a>' % \
             (reverse('show_event', args=[obj.slug]),
              obj.slug)
-    slug_display.verbose_name = _(u'Link')
+    slug_display.short_description = _(u'Internal link')
     slug_display.allow_tags = True
+
+    def queryset(self, request):
+        qs = super(EventAdmin, self).queryset(request)
+
+        if not request.user.has_perm('plus.can_moderate_all'):
+            qs = qs.filter(creator=request.user)
+
+        return qs
+
 
 admin.site.register(Event, EventAdmin)
--- src/plus/models.py
+++ src/plus/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from imagekit.models import ImageModel
 
 
@@ -30,21 +30,31 @@ def random_slug():
 class Event(ImageModel):
     id = models.AutoField(primary_key = True)
 
-    slug = models.SlugField(unique=True, max_length=40, default=random_slug)
-    language = models.IntegerField(max_length=2, choices=LANG_CHOICES)
-    creator = models.ForeignKey(User)
+    slug = models.SlugField(unique=True, max_length=40,
+                            default=random_slug, verbose_name=_(u'Internal ink'))
+    language = models.IntegerField(max_length=2, choices=LANG_CHOICES,
+                                   verbose_name=_(u'Language'))
+    creator = models.ForeignKey(User, verbose_name=_(u'Creator'))
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    starts_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name=_(u'Created at'))
+    starts_at = models.DateTimeField(verbose_name=_(u'Starts at'))
 
-    title = models.CharField(max_length=150, default='')
-    description = models.TextField(default='')
-    url = models.CharField(max_length=256, null=True, blank=True)
-    logo = models.ImageField(null=True, blank=True, upload_to=UPLOAD_DIR)
-    seats_number = models.IntegerField(max_length=5, null=True, blank=True, default=None)
+    title = models.CharField(max_length=150, default='',
+                             verbose_name=_(u'Title'))
+    description = models.TextField(default='',
+                                   verbose_name=_(u'Description'))
+    url = models.CharField(max_length=256, null=True, blank=True,
+                           verbose_name=_('External link'))
+    logo = models.ImageField(null=True, blank=True, upload_to=UPLOAD_DIR,
+                             verbose_name=_(u'Poster'))
+    seats_number = models.IntegerField(max_length=5, null=True,
+                                       blank=True, default=None,
+                                       verbose_name=_(u'Number of seats'))
 
     def __unicode__(self):
         return self.title
+
     class Meta:
         permissions = (
             ("can_moderate_all", _(u"Can add, edit or delete any event")),

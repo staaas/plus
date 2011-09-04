219c83d4fab1ccba1aff3c7093c07ef2ed4a0436
nott <dev@nott.cc>
2011-09-04 20:47:44 +0300

    Enabling avatarizator service.

--- src/commonutils/social/socialize.py
+++ src/commonutils/social/socialize.py
@@ -1,7 +1,26 @@
+from datetime import datetime
+from hashlib import sha1
+
 from django.conf import settings
 from social_auth.models import UserSocialAuth
 
 DEFAULT_AVATAR = getattr(settings, 'DEFAULT_SOCIAL_AVATAR', '')
+
+def get_avatarizator_key():
+    key_src = settings.AVATARIZATOR_KEY + datetime.now().strftime('%Y%m%d%H')
+    return sha1(key_src).hexdigest()
+
+def get_avatarizator_link(provider, uid, key=None):
+    '''
+    Link to avatarizator service.
+    '''
+    if not provider or not uid or not uid.isdigit():
+        return DEFAULT_AVATAR
+    if key is None:
+        key = get_avatarizator_key()
+    return settings.AVATARIZATOR_URL % {'provider': provider,
+                                        'uid': uid,
+                                        'key': key}
 
 def socialize_users(users_list):
     '''
@@ -16,14 +35,16 @@ def socialize_users(users_list):
                 user__id__in=user_ids).select_related())
     soc_dict = {s.user.id: s for s in soc_list}
 
+    avatarizator_key = get_avatarizator_key()
+    
     for usr in users_list:
         soc = soc_dict.get(usr.id)
 
         # default values
         soc_username = usr.username
         soc_link = ''
-        soc_avatar = DEFAULT_AVATAR
         soc_provider = ''
+        soc_uid = ''
 
         if soc is None:
             pass
@@ -35,31 +56,28 @@ def socialize_users(users_list):
             soc_username = screen_name or usr.username
             soc_link = 'http://twitter.com/%s' % screen_name if \
                 screen_name else ''
-            soc_avatar = 'http://img.tweetimag.es/i/%s' % screen_name if \
-                screen_name else DEFAULT_AVATAR
+            soc_uid = soc.uid
             soc_provider = soc.provider
         elif soc.provider == 'facebook':
             soc_username = ('%s %s' % (usr.first_name, usr.last_name)).strip() or usr.username
             soc_link = 'https://www.facebook.com/profile.php?id=%s' % soc.uid
-            soc_avatar = 'http://graph.facebook.com/%s/picture' % soc.uid
+            soc_uid = soc.uid
             soc_provider = soc.provider
         elif soc.provider == 'vkontakte-oauth2':
             soc_username = ('%s %s' % (usr.first_name, usr.last_name)).strip() or usr.username
             soc_link = 'https://vkontakte.ru/id%s' % soc.uid
             soc_provider = 'vkontakte'
-            try:
-                soc_avatar = soc.extra_data.get('response', {}).get('user_photo') or DEFAULT_AVATAR
-            except (TypeError, ValueError, AttributeError):
-                soc_avatar = DEFAULT_AVATAR
+            soc_uid = soc.uid
             usr.vk_id = soc.uid
         elif soc.provider == 'openid':
             soc_username = ('%s %s' % (usr.first_name, usr.last_name)).strip() or usr.username
             soc_link = soc.uid
+            soc_uid = soc.uid
             soc_provider = soc.provider
 
         usr.soc_username = soc_username
         usr.soc_link = soc_link
-        usr.soc_avatar = soc_avatar
+        usr.soc_avatar = get_avatarizator_link(soc_provider, soc_uid, avatarizator_key)
         usr.soc_provider = soc_provider
 
     return users_list

0071a131f1f263fc5fcadddc64107eee2591c732
Stas Rudakou <dev@nott.cc>
2011-10-13 21:12:19 +0300

    Add simple security fields to feedback form

--- src/feedback/views.py
+++ src/feedback/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.template import loader
 from django.http import HttpResponse, Http404
 from pyres import ResQ
@@ -5,21 +7,27 @@ from pyres import ResQ
 from feedback.forms import FeedbackForm
 from feedback.tasks import FeedbackTask
 
+
+logger = logging.getLogger(__name__)
+
 def submit_feedback(request):
     if request.method != 'POST':
         raise Http404
 
-    form = FeedbackForm(request.POST or None)
-    if form.is_valid():
-        r = ResQ()
-        r.enqueue(FeedbackTask, form.cleaned_data['name'],
-                  form.cleaned_data['email'], form.cleaned_data['text'])
-        return HttpResponse(status=200)
+    try:
+        form = FeedbackForm(request.POST or None)
+        if form.is_valid():
+            r = ResQ()
+            r.enqueue(FeedbackTask, form.cleaned_data['name'],
+                      form.cleaned_data['email'], form.cleaned_data['text'])
+            return HttpResponse(status=200)
     
-    # form is invalid
-    return HttpResponse(
-        loader.render_to_string('feedback/feedback_form_partial.html',
-                                {'feedback_form': form}),
-        status=409)
-    
-    
+        # form is invalid
+        return HttpResponse(
+            loader.render_to_string('feedback/feedback_form_partial.html',
+                                    {'feedback_form': form}),
+            status=409)
+    except Exception:
+        logger.exception('')
+        raise
+
